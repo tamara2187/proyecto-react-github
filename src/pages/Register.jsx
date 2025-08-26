26527dfa510bf79accac4f574c1f2129095f714d
tamara encinas <tamaraencinast05@gmail.com>
2025-08-26 07:34:24 -0300

    add:contenido estatico en el Register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -8,7 +8,24 @@ return(
 
 
 <h1>Bienvenidos a mi Register</h1>
+     <form>
+        <div>
+          <label htmlFor="nombre">Nombre:</label>
+          <input type="text" id="nombre" name="nombre" placeholder="Tu nombre" />
+        </div>
 
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" placeholder="ejemplo@correo.com" />
+        </div>
+
+        <div>
+          <label htmlFor="password">Contraseña:</label>
+          <input type="password" id="password" name="password" placeholder="********" />
+        </div>
+
+        <button type="submit">Registrarse</button>
+      </form>
 
 </Layout>
 )
