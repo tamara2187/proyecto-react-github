56187952af6a6976202c0a262465c2cbbd8f4d9d
tamara encinas <tamaraencinast05@gmail.com>
2025-08-26 07:45:08 -0300

    add:modifique mi comp NotFound ,le agregue la url del Home

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,13 +1,14 @@
+import { Link } from "react-router-dom"
 import { Layout } from "../components/Layout"
 //este comp es para cuando buscas una url que no exista dentro de esta pag web , y te aparece esto.
 const NotFound=()=>{
 return(
 <Layout>
   <h1>Esta pagina que buscaba no existe</h1>  
-
+ <Link to="/"> ir a inicio</Link>
  </Layout> 
 )
-
+//no hace falta importar elñ comp Link porque ya lo gice en mi header y ya esta en mi routerApp .
 
 
 
