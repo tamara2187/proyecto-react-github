4b35c14b2c0db826f5f6463ee35eaef2596076fd
tamara encinas <tamaraencinast05@gmail.com>
2025-08-26 07:30:55 -0300

    add:contenido estatico en mi Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,6 +9,21 @@ return(
 
 <h1>Bienvenidos a mi Login</h1>
 
+<p>Ingresa tu correo y contraseña para acceder a tu cuenta.</p>
+
+      <form>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" placeholder="ejemplo@correo.com" />
+        </div>
+
+        <div>
+          <label htmlFor="password">Contraseña:</label>
+          <input type="password" id="password" name="password" placeholder="********" />
+        </div>
+
+        <button >Ingresar</button>
+      </form>
 
 </Layout>
 )
